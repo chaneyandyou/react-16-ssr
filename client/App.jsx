0e1e730596697afa40328d7651fc9c77f9b4ba75
chaneyandyou <chaneychan@126.com>
2018-01-19 23:32:40 +0800

    init project

---
+++ client/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => <div>kljadf家好jl</div>;
+
+// class App extends React.PrueComponent {
+//   render() {
+//     return (
+//       <div>
+//         gogogo
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const favicon = require('serve-favicon')
+const ReactSSR = require('react-dom/server')
+const fs = require('fs')
+const path = require('path')
+
+const isDev = process.env.NODE_ENV === 'development'
+const app = express()
+
+app.use(favicon(path.join(__dirname, '../favicon.ico')))
+
+if (!isDev) {
+  const serverEntry = require('../dist/server-entry').default
+  const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf-8')
+  app.use('/public', express.static(path.join(__dirname, '../dist')))
+  app.get('*', function (req, res) {
+    const appString = ReactSSR.renderToString(serverEntry)
+    res.send(template.replace('<!--app-->', appString))
+  })
+} else {
+  const devStatic = require('./utils/dev-static')
+  devStatic(app)
+}
+
+app.listen(3333, function () {
+  console.log('服务启动了')
+})
